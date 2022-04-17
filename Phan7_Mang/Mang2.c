2d8ac2dc9cc721bf61ad88ce31a2f86144471f65
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-17 14:28:00 +0900

    tinh tong mang

---
+++ Phan7_Mang/Mang2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int a[10];
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int sum=0;
+    for(int i=0;i<10;i++)
+    {
+        sum = sum + a[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
