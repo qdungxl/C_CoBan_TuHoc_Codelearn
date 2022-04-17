c84d468c1d28daa67db84abc34db00a49809fe33
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-17 16:45:16 +0900

    chuyen chuoi thanh chu thuong

---
+++ Phan8_Chuoi/Chuoi9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char s[100];
+    scanf("%s",s);
+    for(int i=0;i<strlen(s);i++)
+    {
+        if(s[i]>='A'&&s[i]<='Z')
+        {
+            s[i]+=32;
+        }
+    }
+    printf("%s",s);
+    return 0;
+}
