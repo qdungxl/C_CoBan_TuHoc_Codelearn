7c76d0d6cf1cd465e35b67d81385cad7a67517ba
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-17 16:07:49 +0900

    ten, dia chi

---
+++ Phan8_Chuoi/Chuoi2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+	char name[50];
+	char address[250];
+	scanf("%s",name);
+	scanf("%s",address);
+	printf("Name: %s\n",name);
+	printf("Address: %s\n",address);
+	return 0;
+}
