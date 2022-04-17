1fccf903e27ae9f96f3ee83a23213e269e781205
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-17 11:42:43 +0900

    tinh tong so le tu 0 den n

---
+++ Phan5_VongLap/for5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int sum =0;
+    for(int i=0;i<=n;i++)
+    {
+        if(i&1)
+        {
+            sum = sum + i;
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
