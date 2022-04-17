2f3e2ae84728b4a5504dea0e2defdff9e87b157a
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-04-17 09:27:56 +0900

    Xuat ra man hinh console

--- ChuongTringDauTien_1/main.c
+++ ChuongTringDauTien_1/main.c
@@ -4,6 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!");
+	printf("Hello World!\n");
+	printf("%d * %d = %d",324,355,324*355);
 	return 0;
 }
